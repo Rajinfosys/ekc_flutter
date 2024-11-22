86679196f18e32558fb83b33b2b7e0d22bbf371e
mehlam52 <mehlan33hamid@gmail.com>
2024-11-22 17:07:29 +0530

    base api issue fixes

--- lib/presentation/scan_serial/controller/serial_controller.dart
+++ lib/presentation/scan_serial/controller/serial_controller.dart
@@ -38,7 +38,7 @@ class ScanSerialController extends GetxController {
 
   static ScanSerialController get instance => Get.find<ScanSerialController>();
 
-  static const String _getCommonPath = '/scriptcase/app/ekc_qc/api_qrcode/index.php';
+  static const String _getCommonPath = '/api_qrcode/index.php';
 
   void updateSerial() async {
     try {
